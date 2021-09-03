15dc7b952c82510147137d31dbf7e8c433fb118d
Dan Klishch <danilklishch@gmail.com>
2021-09-03 19:33:42 +0500

    basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cpp-protoc-initializers
-Protocol Buffers Compiler (protoc) plugin allowing aggregate initialization on generated types in C++
+
+**WORK IN PROGRESS**
+
+This plugin allows using aggregate initialization on Protocol Buffer generated classes.
+
+Invocation: `protoc --cpp_out=. --cpp_initializers_out=. PROTO_FILES`.
+
+Example:
+```protobuf
+syntax = "proto3";
+
+package foo;
+
+message Message {
+	int32 field;
+}
+```
+
+```cpp
+template <typename T>
+void foo(const typename T::initializable_type &message_) {
+	T message(message_);
+}
+
+foo<package::Message>({.field = 2});
+```
---
+++ src/main.cc
@@ -0,0 +1,53 @@
+#include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/printer.h>
+using namespace google::protobuf;
+
+class Generator : public compiler::CodeGenerator {
+private:
+	bool GenerateFor(const Descriptor *message, const FileDescriptor *file,
+					 compiler::GeneratorContext *context) const {
+		auto class_scope_inserter = context->OpenForInsert(
+			compiler::StripProto(file->name()) + ".pb.h", "class_scope:" + message->full_name());
+		auto printer = io::Printer(class_scope_inserter, '$');
+
+		printer.Print("struct initializable_type {\n");
+		printer.Indent();
+		for (int i = 0; i < message->field_count(); ++i) {
+			auto field = message->field(i);
+			printer.Print("::PROTOBUF_NAMESPACE_ID::$type$ $name$;\n", "type",
+						  field->cpp_type_name(), "name", field->name().c_str());
+		}
+		printer.Outdent();
+		printer.Print("};\n\n$constructor$(const initializable_type &t) {\n", "constructor",
+					  message->name());
+		printer.Indent();
+		for (int i = 0; i < message->field_count(); ++i) {
+			auto field = message->field(i);
+			printer.Print("set_$lname$(t.$name$);\n", "lname", field->lowercase_name().c_str(),
+						  "name", field->name().c_str());
+		}
+		printer.Outdent();
+		printer.Print("}\n");
+
+		for (int i = 0; i < message->nested_type_count(); ++i) {
+			GenerateFor(message->nested_type(i), file, context);
+		}
+		return true;
+	}
+
+public:
+	bool Generate(const FileDescriptor *file, const string &parameter [[maybe_unused]],
+				  compiler::GeneratorContext *context, string *error [[maybe_unused]]) const {
+		for (int i = 0; i < file->message_type_count(); ++i) {
+			GenerateFor(file->message_type(i), file, context);
+		}
+		return true;
+	}
+};
+
+int main(int argc, char **argv) {
+	Generator generator;
+	return compiler::PluginMain(argc, argv, &generator);
+}
